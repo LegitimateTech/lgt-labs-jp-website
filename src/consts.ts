0106a68e94701cbccd52cd45c40da7c021b7749c
Calvin <calvin.chan.h@gmail.com>
2025-06-18 17:03:34 +0900

    first commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Legitimate Labs JP合同会社";
+export const SITE_DESCRIPTION = `レジティメイトラボスジェーピーゴウドウガイシャは、NFCテクノロジー、ICチップ、コネクテッドプロダクト、ブロックチェーンソフトウェアに特化したコンサルティングおよび開発を行う企業です。また、クリエイティブ・コンサルティングも手がけ、ブランドとテクノロジーの融合を支援します。
+
+主に、米国法人Legitimate, Inc.の日本国内およびアジア市場における事業展開を支援するとともに、日本国内のファッションブランド、音楽レーベル、その他多様な企業とも連携し、最先端のNFC／ICチップ技術を活用した魅力的なコネクテッドプロダクトの企画・開発・展開をサポートしています。
+`
